787b7ac3e5408ef95190cd8d7b397bcb61838d74
LaT1K <sashalatkivskiy@gmail.com>
2024-03-12 18:55:24 +0200

    update

---
+++ OnlineShop/RegisterForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data.SqlTypes;
+
+namespace OnlineShop {
+    public partial class RegisterForm : Form {
+        private MySqlConnection con = null;
+        private string connstring = null;
+        public RegisterForm() {
+            InitializeComponent();
+            this.Text = "Registration";
+            connstring = "server=localhost;user=root;password=admin;database=online_shop";
+            con = new MySqlConnection(connstring);
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e) {
+            textBox2.UseSystemPasswordChar = true;
+            textBox3.UseSystemPasswordChar = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            registerUser();
+        }
+        private bool doesAlreadyExist(string login) {
+            con.Open();
+            string queryString = "SELECT COUNT(*) FROM customers WHERE login = @login";
+            using(MySqlCommand cmd = new MySqlCommand(queryString, con)) {
+                cmd.Parameters.AddWithValue("@login", textBox1.Text);
+                int existringUserCount = Convert.ToInt32(cmd.ExecuteScalar());
+                if(existringUserCount > 0) {
+                    MessageBox.Show("The user already exists");
+                    return true;
+                }
+                else
+                    return false;
+            }
+            
+        }
+        private void registerUser() {
+            string queryString = "INSERT INTO customers(login, password) VALUES (@login, @password)";
+            if(!doesAlreadyExist(textBox1.Text) && textBox2.Text == textBox3.Text) {
+                using (MySqlCommand cmd = new MySqlCommand(queryString, con)) {
+                    cmd.Parameters.AddWithValue("@login", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@password", textBox3.Text);
+
+                    try {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("You were succesfully registered");
+                    }
+                    catch(MySqlException ex) {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
